785d6ab16351935d1b8b9ed86af418cec2e4d4eb
neptunepink <purpleposeidon@gmail.com>
2013-11-27 17:56:28 -0800

    Pull build.py over

---
+++ build.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import os, subprocess
+
+def process(inputname, outputname):
+  print(inputname)
+  out = subprocess.Popen(["markdown", inputname], stdout=subprocess.PIPE).stdout.read()
+  fd = open(outputname, 'w')
+  fd.write(
+"""<html>
+<head>
+  <title>Factorization Documentation</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+""")
+  fd.write(out)
+  fd.write(
+"""
+</body>
+</html>
+""")
+  fd.close()
+
+for baseName, subDirs, files in os.walk("src/"):
+  for inputname in files:
+    outputname = (inputname.rstrip(".md") + ".html").lstrip("src/")
+    inputname = baseName + inputname
+    process(inputname, outputname)
